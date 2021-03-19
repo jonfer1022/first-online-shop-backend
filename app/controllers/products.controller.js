ae92b36ca00c4fd6f549572d570efbb57625b54d
jonfer1022 <75956299+jonfer1022@users.noreply.github.com>
2021-03-18 22:45:13 -0500

    Modificación para más filtros en el servicio de getAllProducts

--- app/controllers/products.controller.js
+++ app/controllers/products.controller.js
@@ -2,35 +2,14 @@ const ProductsModel = rootRequire('app/models/products.model')
 
 const getAllProducts = async (req,res) =>{
   try {
-    const { gender, category, sortBy} = req.query;
-    let data = await ProductsModel.getAllProducts(gender, category, sortBy);
+    const { gender, category, sortBy, size, priceMin, priceMax} = req.query;
+    let data = await ProductsModel.getAllProducts(gender, category, sortBy, size, priceMin, priceMax);
     res.json(data)
   } catch (error) {
     console.error("An error ocurred getAllProducts: ",error);
     throw ("An error ocurred getAllProducts: ",error);
   }
 }
-
-// const getDiscountClothes = async (req,res) =>{
-//   try {
-//     let data = await homeModel.getDiscountClothes();
-//     res.json(data)
-//   } catch (error) {
-//     console.error("An error ocurred getDiscountClothes: ",error);
-//     throw ("An error ocurred getDiscountClothes: ",error);
-//   }
-// }
-
-// const getLastestCollection = async (req,res) =>{
-//   try {
-//     const { amount } = req.query;
-//     let data = await homeModel.getLastestCollection(amount);
-//     res.json(data)
-//   } catch (error) {
-//     console.error("An error ocurred getLastestCollection: ",error);
-//     throw ("An error ocurred getLastestCollection: ",error);
-//   }
-// }
 
 module.exports = {
   getAllProducts
--- app/models/products.model.js
+++ app/models/products.model.js
@@ -3,7 +3,7 @@ const master_data = require ('../master_data');
 
 class ProductsModel {
 
-  static async getAllProducts(gender, category, sortBy){
+  static async getAllProducts(gender, category, sortBy, size, priceMin, priceMax){
     return new Promise(async (resolve, reject) =>{
       try {
         if(connection){
@@ -12,19 +12,24 @@ class ProductsModel {
               c.*,
               im.image_path,
               cz.price,
+              s.name,
               IF(dc.discount_flag = ${true}, p.percentage, ${null}) as percentage
             from clothes c
             inner join images_clothes im ON im.id_clothes = c.id
             inner join clothes_sizes cz ON cz.id = c.id 
+            inner join _sizes s ON s.id = cz.id_sizes
             left join discount_clothes dc ON dc.id_clothes = c.id
             left join _percentage p ON p.id = dc.id_percentage
             where 
               im.principal_flag = ${true} 
+              AND cz.price BETWEEN ${priceMin} AND ${priceMax}
               ${ gender.length > 0 ? `AND c.gender_id = '${gender}'` : "AND c.gender_id IS NOT NULL"} 
               ${ category.length > 0 ? `AND c.category_id = ${category}` : "AND c.category_id IS NOT NULL"}
-              ${ sortBy == master_data.sortBy.bestDiscount ? `AND dc.discount_flag = ${true} ORDER BY dc.id_percentage DESC` :""}
-              ${ sortBy == master_data.sortBy.lowerPrice ? `ORDER BY cz.price ASC` : ""}
-              ${ sortBy == master_data.sortBy.higherPrice ? `ORDER BY cz.price DESC` : ""}
+              ${ size.length > 0 ? `AND cz.id_sizes IN (${size})`:" AND cz.id_sizes IS NOT NULL"}
+            ${ sortBy == master_data.sortBy.lowerPrice ? `ORDER BY cz.price ASC` : ""}
+            ${ sortBy == master_data.sortBy.higherPrice ? `ORDER BY cz.price DESC` : ""}
+            ${ sortBy == master_data.sortBy.bestDiscount ? `AND dc.discount_flag = ${true} ORDER BY dc.id_percentage DESC` :""}
+            ${ sortBy == master_data.sortBy.lastlestCollection ? `ORDER BY c.created_at DESC`:``}
             `;
           // console.log(querySql);
           connection.query(querySql, (error, rows) =>{
@@ -38,61 +43,6 @@ class ProductsModel {
       }
     })
   };
-
-  /*
-  static async getDiscountClothes(){
-    return new Promise(async (resolve, reject) =>{
-      try {
-        if(connection){
-          let querySql = `
-            select 
-              dc.*, 
-              c.product_name,
-              p.percentage,
-              ic.image_path
-            from discount_clothes dc
-            inner join clothes c ON c.id = dc.id_clothes
-            inner join _percentage p ON p.id = dc.id_percentage
-            inner join images_clothes ic ON ic.id_clothes = c.id
-            where dc.discount_flag = ${true} AND ic.principal_flag = ${true}
-            `;
-          connection.query(querySql, (error, rows) =>{
-            if(error) reject(`An error ocurred in the query getDiscountClothes: ${error}`);
-            resolve (rows);
-          })
-        }
-      } catch (error) {
-        console.error(`An error ocurred in getDiscountClothes: ${error}`);
-        reject(`An error ocurred in getDiscountClothes: ${error}`);
-      }
-    })
-  };
-
-  static async getLastestCollection(amount){
-    return new Promise(async (resolve, reject) =>{
-      try {
-        if(connection){
-          let querySql = `
-            select 
-              c.*,
-              im.image_path
-            from clothes c
-            inner join images_clothes im ON im.id_clothes = c.id
-            where im.principal_flag = ${true} AND c.amount > ${0}
-            order by created_at DESC limit 0 , ${amount}
-            `;
-          connection.query(querySql, (error, rows) =>{
-            if(error) reject(`An error ocurred in the query getLastestCollection: ${error}`);
-            resolve (rows);
-          })
-        }
-      } catch (error) {
-        console.error(`An error ocurred in getLastestCollection: ${error}`);
-        reject(`An error ocurred in getLastestCollection: ${error}`);
-      }
-    })
-  };
-  */
 }
 
 module.exports = ProductsModel;
